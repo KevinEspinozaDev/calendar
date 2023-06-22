96a4a247c9517fb1296656054298d3ee3a12cd58
Kevin Espinoza <kevinespinozadev@gmail.com>
2023-06-22 01:46:11 -0300

    Login, logout, register y rutas protegidas según token

--- src/apis/calendarApi.js
+++ src/apis/calendarApi.js
@@ -7,6 +7,14 @@ const calendarApi = axios.create({
     baseURL: VITE_API_URL
 });
 
-// Todo: Configurar interceptors
+calendarApi.interceptors.request.use(config => {
+
+    config.headers = {
+        ...config.headers,
+        'x-token': localStorage.getItem('token')
+    }
+
+    return config;
+})
 
 export default calendarApi;
--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import {calendarApi} from './../apis';
+import {clearErrorMessage, onChecking, onLogin, onLogout} from './../store';
 
 export const useAuthStore = () => {
 
@@ -7,17 +8,77 @@ export const useAuthStore = () => {
     const dispatch = useDispatch();
 
     const startLogin = async({email, password}) => {
+
+        dispatch(onChecking());
         
         const url = '/auth';
 
         try {
-            const resp = await calendarApi.post(url, {email, password});
-            console.log({resp});
+            const {data} = await calendarApi.post(url, {email, password});
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('token-init-date', new Date().getTime());
+
+            dispatch(onLogin({name: data.name, uid: data.uid}));
 
         } catch (error) {
-            console.log(error);
-        }
+            dispatch(onLogout('Wrong authentication.'));
 
+            setTimeout(() => {
+                dispatch(clearErrorMessage());
+            }, 10);
+        }
+    }
+
+    const startRegister = async({name, email, password}) => {
+
+        dispatch(onChecking());
+
+        const url = '/auth/new';
+
+        try {
+            const {data} = await calendarApi.post(url, {name, email, password});
+
+            if (data) {
+                localStorage.setItem('token', data.token);
+                localStorage.setItem('token-init-date', new Date().getTime());
+
+                dispatch(onLogin({name: data.name, uid: data.uid}));
+            }
+
+        } catch (error) {
+            
+            dispatch(onLogout(error.response.data?.msg || 'Error al registrar usuario.'));
+
+            setTimeout(() => {
+                dispatch(clearErrorMessage());
+            }, 10);
+        }
+    }
+
+    const checkAuthToken = async() => {
+
+        const url = 'auth/renew';
+
+        const token = localStorage.getItem('token');
+        if (!token) return dispatch(onLogout());
+
+        try {
+            const {data} = await calendarApi.get(url);
+
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('token-init-date', new Date().getTime());
+
+            dispatch(onLogin({name: data.name, uid: data.uid}));
+
+        } catch (error) {
+            localStorage.clear();
+            dispatch(onLogout());
+        }
+    }
+
+    const startLogout = () => {
+        localStorage.clear();
+        dispatch(onLogout());
     }
 
 
@@ -29,5 +90,8 @@ export const useAuthStore = () => {
 
         // Methods
         startLogin,
+        startRegister,
+        checkAuthToken,
+        startLogout
     }
 }
